e803642a9edbe6710d5f7cd69e36653d00d5fc8b
Samson Cheung <samsonc89@yahoo.com>
2022-09-29 12:43:23 -0700

    Add svg to new book part of function

--- script.js
+++ script.js
@@ -104,7 +104,11 @@ function addElem(book) {
   <span class="slider round"></span>
   </label> <p class='label-read'>Read</p>
   </div> |
-  <button class="delete">Delete</button>
+  <button class="delete">
+  <svg class='delete-svg' style="width:22px;height:22px" viewBox="0 0 24 24">
+    <path fill="currentColor" d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
+    </svg>
+  </button>
   </div>`;
 
   cardContainer.insertAdjacentHTML("beforeend", html);
