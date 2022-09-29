196eb5cbedc39ffb72dd45ccf2733ca51c5cdfce
Samson Cheung <samsonc89@yahoo.com>
2022-09-29 13:11:09 -0700

    Fix delete button class

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ function addElem(book) {
   <span class="slider round"></span>
   </label> <p class='label-read'>Read</p>
   </div> |
-  <button class="delete">
+  <button class="delete-btn">
   <svg class='delete-svg' style="width:22px;height:22px" viewBox="0 0 24 24">
     <path fill="currentColor" d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
     </svg>
